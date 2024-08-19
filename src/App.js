764921946cda46bdd931c365a272d114b8e2cbc5
Felipe Costa <f1.costa2022@gmail.com>
2024-08-19 20:38:33 -0300

    message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Esse é meu Primeiro App.
+          My Deployment With AWS CodePipeline
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Cloud Treinamentos
+          Phill
         </a>
       </header>
     </div>
